6c988ef889c46bf46fda409a0beb2197ba25b2c2
Fuad <fuadhasanfhm@gmail.com>
2020-04-28 14:02:50 +0600

    banner and interstital Ads

---
+++ Ads/app/src/main/java/com/example/ads/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    Button show;
+   private AdView adView;
+    InterstitialAd interstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        show = findViewById(R.id.b1);
+        adView = findViewById(R.id.adview1);
+
+        MobileAds.initialize(this, "ca-app-pub-1455584119366220~8047334923");
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                interstitialAd.show();
+            }
+        });
+    }
+}
